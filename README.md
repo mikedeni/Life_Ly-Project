4a7ff4ea4a8571de3ec0ebbb4cdd8ce3b351ee2a
MIKEDENI <nam74102@gmail.com>
2025-03-03 02:37:56 +0700

    Refactor UI and improve code structure

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Life_Ly-Project
 
-/* 
-  โปรแกรมคำนวณความคุ้มค่าของสินค้า
-  เป็นโปรเจ็คจบคอร์ด JavaScript ของ Born to Dev.
-*/
+โปรแกรมคำนวณความคุ้มค่าของสินค้า เป็นโปรเจ็คจบคอร์ด JavaScript ของ Born to Dev.
+
+## Screenshots
+
+![App Screenshot](https://media.discordapp.net/attachments/1064734103297073192/1345842185941286933/image.png?ex=67c604b5&is=67c4b335&hm=4bccf2e3cac271adb87dbc0438b0e0be362378b900350381a5297158c31f07b7&=&format=webp&quality=lossless&width=1794&height=884)
--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 // ราคา / ปริมาตร
 function toValue(price, earn) {
-    let value = price / earn
-    return value
+    let value = price / earn;
+    return value.toFixed(2);
 }
 
 function display(elementID, value) {
-    document.getElementById(elementID).innerHTML = value
+    document.getElementById(elementID).innerHTML = value;
 }
 
 function toWorthinessPrograme() {
-    let A = toValue(document.getElementById('priceA').value, document.getElementById('earnA').value);
-    let B = toValue(document.getElementById('priceB').value, document.getElementById('earnB').value);
+    let priceA = document.getElementById("priceA").value;
+    let earnA = document.getElementById("earnA").value;
+    let priceB = document.getElementById("priceB").value;
+    let earnB = document.getElementById("earnB").value;
 
-    if (A > B) {
-        display('result', `สินค้าชิ้นที่ 1 : ${A.toFixed(2)} บาท/ปริมาตร <br>
-        สินค้าชิ้นที่ 2 : ${B.toFixed(2)} บาท/ปริมาตร <br>
-        **ดังนั้น สินค้าชิ้นที่ 2 คุ้มกว่านะ 🎉`)
-    } else {
-        display('result', `สินค้าชิ้นที่ 1 : ${A.toFixed(2)} บาท/ปริมาตร <br>
-        สินค้าชิ้นที่ 2 : ${B.toFixed(2)} บาท/ปริมาตร <br>
-        **ดังนั้น สินค้าชิ้นที่ 1 คุ้มกว่านะ 🎉`)
+    if (!priceA || !earnA || !priceB || !earnB) {
+        alert("❗ กรุณากรอกข้อมูลให้ครบถ้วนทั้งสองชุด");
+        return;
     }
-};
+
+    let A = toValue(priceA, earnA);
+    let B = toValue(priceB, earnB);
+
+    alert(
+        `✨ สินค้าชุดที่ ${
+            A > B ? "2" : "1"
+        } คุ้มกว่านะ 💵\n📜 สินค้าชิ้นที่ 1 : ${A} บาท/ปริมาตร\n📜 สินค้าชิ้นที่ 2 : ${B} บาท/ปริมาตร`
+    );
+}
